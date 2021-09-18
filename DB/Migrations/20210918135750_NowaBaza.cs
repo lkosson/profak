67983b255b1d39615a35831855caf3dafa7b8e49
Łukasz Kosson <git@lukasz.kosson.net>
2021-09-18 15:58:15 +0200

    WIP

---
+++ DB/Migrations/20210918135750_NowaBaza.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProFak.DB.Migrations
+{
+    public partial class NowaBaza : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
