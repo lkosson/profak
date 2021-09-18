dd9f6759fdb3d9886006c384d9f93e7b0d7df065
Łukasz Kosson <git@lukasz.kosson.net>
2021-09-18 08:46:38 +0200

    WIP

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProFak
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+		}
+	}
+}
