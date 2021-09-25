44bdda24390cbdf114a7401e5e7ae1ce7d047dc7
Łukasz Kosson <git@lukasz.kosson.net>
2021-09-25 19:33:30 +0200

    WIP

---
+++ DB/Migrations/20210925173314_KluczeGlowne.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProFak.DB.Migrations
+{
+    public partial class KluczeGlowne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Faktura_Kontrahent_SprzedawcId",
+                table: "Faktura");
+
+            migrationBuilder.RenameColumn(
+                name: "SprzedawcId",
+                table: "Faktura",
+                newName: "SprzedawcaId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Faktura_SprzedawcId",
+                table: "Faktura",
+                newName: "IX_Faktura_SprzedawcaId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Waluta",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Towar",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "StawkaVat",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "SposobPlatnosci",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "PozycjaFaktury",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Kontrahent",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "JednostkaMiary",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Faktura",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Faktura_Kontrahent_SprzedawcaId",
+                table: "Faktura",
+                column: "SprzedawcaId",
+                principalTable: "Kontrahent",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Faktura_Kontrahent_SprzedawcaId",
+                table: "Faktura");
+
+            migrationBuilder.RenameColumn(
+                name: "SprzedawcaId",
+                table: "Faktura",
+                newName: "SprzedawcId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Faktura_SprzedawcaId",
+                table: "Faktura",
+                newName: "IX_Faktura_SprzedawcId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Waluta",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Towar",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "StawkaVat",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "SposobPlatnosci",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "PozycjaFaktury",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Kontrahent",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "JednostkaMiary",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Faktura",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Faktura_Kontrahent_SprzedawcId",
+                table: "Faktura",
+                column: "SprzedawcId",
+                principalTable: "Kontrahent",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
