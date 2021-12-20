333f5472f7e676693d1644448285601fdbc0da6d
Łukasz Kosson <git@lukasz.kosson.net>
2021-12-20 08:40:26 +0100

    WIP

--- Instalator/zbuduj.bat
+++ Instalator/zbuduj.bat
@@ -9,20 +9,20 @@ cd bin\Publish
 
 attrib +r *.dll
 attrib +r *.json
-attrib +r ProFak.exe
+attrib +r *.exe
 attrib +r pl\*.dll
 del /s /q *.* 2> nul >nul
 attrib /s -r *.*
 
 cd ..\..\Instalator
 
-heat dir ..\bin\Publish -sreg -srd -sfrag -gg -template fragment -cg Pliki -var var.Zrodlo -dr KatalogProgramu -out ProFak-pliki.wxs
+heat dir ..\bin\Publish -sreg -srd -sfrag -gg -template fragment -cg Pliki -var var.Zrodlo -dr KATALOGPROGRAMU -out ProFak-pliki.wxs
 if errorlevel 1 goto :blad
 
 candle.exe ProFak.wxs ProFak-pliki.wxs -dZrodlo=..\bin\Publish\
 if errorlevel 1 goto :blad
 
-light.exe ProFak.wixobj ProFak-pliki.wixobj -out ..\bin\Install\ProFak.msi
+light.exe ProFak.wixobj ProFak-pliki.wixobj -ext WixUIExtension -cultures:pl-PL -out ..\bin\Install\ProFak.msi
 if errorlevel 1 goto :blad
 
 goto :koniec
