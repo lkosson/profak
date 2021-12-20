3c38bbc4ad7426ec9d53d743f56e7dd98ee8b2ba
Łukasz Kosson <git@lukasz.kosson.net>
2021-12-20 13:53:42 +0100

    Import struktur JPK_V7M

---
+++ IO/JPK/regen.bat
@@ -0,0 +1,3 @@
+@echo off
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\xsd" /classes /namespace:ProFak.IO.JPK "Schemat_JPK_V7M(2)_v1-0.xsd" "StrukturyDanych_v8-0E.xsd" "ElementarneTypyDanych_v8-0E.xsd" "KodyUrzedowSkarbowych_v7-0E.xsd" "KodyKrajow_v8-0E.xsd"
+pause
