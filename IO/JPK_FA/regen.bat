d971070d634357de4563ca46511912860ffe5749
Łukasz Kosson <git@lukasz.kosson.net>
2023-09-23 10:47:04 +0200

    Aktualizacja do JPK_FA(4)

--- IO/JPK_FA/regen.bat
+++ IO/JPK_FA/regen.bat
@@ -1,4 +1,11 @@
 @echo off
-"c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\xsd" /classes /namespace:ProFak.IO.JPK_FA "Faktury_VAT_-JPK_FA(3).xsd" "StrukturyDanych_v5-0E.xsd" "ElementarneTypyDanych_v5-0E.xsd" "KodyUrzedowSkarbowych_v5-0E.xsd" "KodyKrajow_v5-0E.xsd" "KodyCechKrajow_v3-0E.xsd"
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\xsd" ^
+/classes ^
+/namespace:ProFak.IO.JPK_FA ^
+Schemat_JPK_FA(4)_v1-0.xsd ^
+StrukturyDanych_v5-0E.xsd ^
+ElementarneTypyDanych_v5-0E.xsd ^
+KodyUrzedowSkarbowych_v5-0E.xsd ^
+KodyKrajow_v5-0E.xsd
 
 pause
