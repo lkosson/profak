1ab1a6294d7629a941d41e38bb87d3e080cc8d96
Łukasz Kosson <git@lukasz.kosson.net>
2024-01-19 07:54:17 +0100

    KSeF URL

--- DB/Faktura.cs
+++ DB/Faktura.cs
@@ -41,6 +41,7 @@ namespace ProFak.DB
 
 		public string NumerKSeF { get; set; } = "";
 		public string XMLKSeF { get; set; } = "";
+		public string URLKSeF { get; set; } = "";
 		public DateTime? DataKSeF { get; set; }
 
 		public int? SprzedawcaId { get; set; }
---
+++ DB/Migrations/20240119065224_URLKSeF.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProFak.DB.Migrations
+{
+    public partial class URLKSeF : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SrodowiskoKSeF",
+                table: "Kontrahent",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<string>(
+                name: "URLKSeF",
+                table: "Faktura",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "URLKSeF",
+                table: "Faktura");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SrodowiskoKSeF",
+                table: "Kontrahent",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldDefaultValue: 0);
+        }
+    }
+}
