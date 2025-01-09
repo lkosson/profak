0dd1ea2e0029a54bcfafe7fcbbd2baa3b77d5be4
Łukasz Kosson <git@lukasz.kosson.net>
2025-01-09 10:21:49 +0100

    #27 - Cofanie numeracji po usunięciu faktury

--- UI/Faktury/UsunFaktureAkcja.cs
+++ UI/Faktury/UsunFaktureAkcja.cs
@@ -12,5 +12,31 @@ namespace ProFak.UI
 	class UsunFaktureAkcja : UsunRekordAkcja<Faktura>
 	{
 		public override bool CzyDostepnaDlaRekordow(IEnumerable<Faktura> zaznaczoneRekordy) => base.CzyDostepnaDlaRekordow(zaznaczoneRekordy) && !zaznaczoneRekordy.Any(faktura => faktura.FakturaKorygujacaRef.IsNotNull);
+
+		protected override void Usun(Kontekst kontekst, IEnumerable<Faktura> zaznaczoneRekordy)
+		{
+			base.Usun(kontekst, zaznaczoneRekordy);
+
+			var numeryFaktur = kontekst.Baza.Faktury.Select(e => e.Numer).ToHashSet();
+
+			foreach (var faktura in zaznaczoneRekordy)
+			{
+				var numerator = kontekst.Baza.Numeratory.FirstOrDefault(numerator => numerator.Przeznaczenie == faktura.Numerator);
+				if (numerator == null) continue;
+				var szablon = Numerator.PrzygotujWzorzec(numerator.Format, faktura.Podstawienie);
+				var parametry = String.Format(szablon, "");
+				var stanNumeratora = kontekst.Baza.StanyNumeratorow.FirstOrDefault(stan => stan.NumeratorId == numerator.Id && stan.Parametry == parametry);
+				if (stanNumeratora == null) continue;
+				var cofniety = false;
+				while (stanNumeratora.OstatniaWartosc > 0)
+				{
+					var testowanyNumer = String.Format(szablon, stanNumeratora.OstatniaWartosc);
+					if (numeryFaktur.Contains(testowanyNumer)) break;
+					stanNumeratora.OstatniaWartosc--;
+					cofniety = true;
+				}
+				if (cofniety) kontekst.Baza.Zapisz(stanNumeratora);
+			}
+		}
 	}
 }
--- UI/UsunRekordAkcja.cs
+++ UI/UsunRekordAkcja.cs
@@ -20,8 +20,13 @@ namespace ProFak.UI
 			using var nowyKontekst = new Kontekst(kontekst);
 			if (MessageBox.Show("Czy na pewno chcesz usunąć zaznaczoną pozycję?", "ProFak", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) != DialogResult.Yes) return;
 			using var transakcja = nowyKontekst.Transakcja();
-			nowyKontekst.Baza.Usun(zaznaczoneRekordy);
+			Usun(nowyKontekst, zaznaczoneRekordy);
 			transakcja.Zatwierdz();
+		}
+
+		protected virtual void Usun(Kontekst kontekst, IEnumerable<TRekord> zaznaczoneRekordy)
+		{
+			kontekst.Baza.Usun(zaznaczoneRekordy);
 		}
 	}
 }
