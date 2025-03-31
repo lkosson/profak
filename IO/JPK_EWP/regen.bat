c922d5410eea070554e8bf9a736ee584dd56ec69
Łukasz Kosson <git@lukasz.kosson.net>
2025-03-31 10:14:29 +0200

    Import struktur dla JPK_EWP i JPK_PKPIR

---
+++ IO/JPK_EWP/regen.bat
@@ -0,0 +1,11 @@
+@echo off
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\xsd" ^
+/classes ^
+/namespace:ProFak.IO.JPK_EWP ^
+Struktura_Schemat_JPK_EWP(4)_v1-0.xsd ^
+StrukturyDanych_v10-0E.xsd ^
+ElementarneTypyDanych_v10-0E.xsd ^
+KodyUrzedowSkarbowych_v8-0E.xsd ^
+KodyKrajow_v10-0E.xsd
+
+pause
