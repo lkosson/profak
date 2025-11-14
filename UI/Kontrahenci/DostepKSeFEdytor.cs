7e0c36af65ec01d7efc02bafd273f28a90e45d83
Łukasz Kosson <git@lukasz.kosson.net>
2025-11-14 08:34:01 +0100

    Poprawiona obsługa błędów przy nadawaniu uprawnień KSeF

--- UI/Kontrahenci/DostepKSeFEdytor.cs
+++ UI/Kontrahenci/DostepKSeFEdytor.cs
@@ -18,47 +18,61 @@ partial class DostepKSeFEdytor : UserControl
 
 	private void buttonPobierzXML_Click(object sender, EventArgs e)
 	{
-		var nip = NIP;
-		string xml = null;
-		OknoPostepu.Uruchom(async delegate
+		try
 		{
-			using var api = new IO.KSEF2.API(SrodowiskoKSeF);
-			xml = await api.AuthenticateSignatureBeginAsync(nip);
-		});
-		if (xml == null) return;
+			var nip = NIP;
+			string xml = null;
+			OknoPostepu.Uruchom(async delegate
+			{
+				using var api = new IO.KSEF2.API(SrodowiskoKSeF);
+				xml = await api.AuthenticateSignatureBeginAsync(nip);
+			});
+			if (xml == null) return;
 
-		using var dialog = new SaveFileDialog();
-		dialog.Filter = "Wniosek o dostęp (*.xml)|*.xml|Wszystkie pliki (*.*)|*.*";
-		dialog.Title = "Wybierz miejsce do zapisu wniosku o dostęp";
-		dialog.RestoreDirectory = true;
-		dialog.FileName = "wniosek-do-podpisu.xml";
-		if (dialog.ShowDialog() != DialogResult.OK) return;
-		File.WriteAllText(dialog.FileName, xml);
-		MessageBox.Show("Wniosek został zapisany pomyślnie. Podpisz go elektronicznie i załaduj do programu zgodnie z dalszymi instrukcjami.", "ProFak", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			using var dialog = new SaveFileDialog();
+			dialog.Filter = "Wniosek o dostęp (*.xml)|*.xml|Wszystkie pliki (*.*)|*.*";
+			dialog.Title = "Wybierz miejsce do zapisu wniosku o dostęp";
+			dialog.RestoreDirectory = true;
+			dialog.FileName = "wniosek-do-podpisu.xml";
+			if (dialog.ShowDialog() != DialogResult.OK) return;
+			File.WriteAllText(dialog.FileName, xml);
+			MessageBox.Show("Wniosek został zapisany pomyślnie. Podpisz go elektronicznie i załaduj do programu zgodnie z dalszymi instrukcjami.", "ProFak", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+		catch (Exception exc)
+		{
+			OknoBledu.Pokaz(exc);
+		}
 	}
 
 	private void buttonWskazXML_Click(object sender, EventArgs e)
 	{
-		using var dialog = new OpenFileDialog();
-		dialog.Filter = "Wniosek o dostęp (*.xml)|*.xml|Wszystkie pliki (*.*)|*.*";
-		dialog.Title = "Wybierz plik z podpisanym wnioskiem o dostęp";
-		dialog.RestoreDirectory = true;
-		if (dialog.ShowDialog() != DialogResult.OK) return;
-
-		var signedXml = File.ReadAllText(dialog.FileName);
-
-		string token = null;
-		OknoPostepu.Uruchom(async delegate
+		try
 		{
-			using var api = new IO.KSEF2.API(SrodowiskoKSeF);
-			await api.AuthenticateSignatureEndAsync(signedXml);
-			token = await api.GenerateToken();
-		});
-		if (token == null) return;
+			using var dialog = new OpenFileDialog();
+			dialog.Filter = "Wniosek o dostęp (*.xml)|*.xml|Wszystkie pliki (*.*)|*.*";
+			dialog.Title = "Wybierz plik z podpisanym wnioskiem o dostęp";
+			dialog.RestoreDirectory = true;
+			if (dialog.ShowDialog() != DialogResult.OK) return;
 
-		Token = token;
-		MessageBox.Show("Dostęp do KSeF nadany pomyślnie. Można skasować utworzone pliki.", "ProFak", MessageBoxButtons.OK, MessageBoxIcon.Information);
-		ParentForm.Close();
+			var signedXml = File.ReadAllText(dialog.FileName);
+
+			string token = null;
+			OknoPostepu.Uruchom(async delegate
+			{
+				using var api = new IO.KSEF2.API(SrodowiskoKSeF);
+				await api.AuthenticateSignatureEndAsync(signedXml);
+				token = await api.GenerateToken();
+			});
+			if (token == null) return;
+
+			Token = token;
+			MessageBox.Show("Dostęp do KSeF nadany pomyślnie. Można skasować utworzone pliki.", "ProFak", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			ParentForm.Close();
+		}
+		catch (Exception exc)
+		{
+			OknoBledu.Pokaz(exc);
+		}
 	}
 
 	private void linkEPUAP_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
