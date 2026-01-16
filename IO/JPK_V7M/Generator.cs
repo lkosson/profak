8fb4d45560587eee4a5cd856cb60c7cee80da39a
Łukasz Kosson <git@lukasz.kosson.net>
2026-01-16 10:01:09 +0100

    Aktualizacja do JPK_V7M(3)

--- IO/JPK_V7M/Generator.cs
+++ IO/JPK_V7M/Generator.cs
@@ -17,10 +17,6 @@ namespace ProFak.IO.JPK_V7M
 		{
 			var jpk = Zbuduj(baza, deklaracja);
 			var xo = new XmlAttributeOverrides();
-			xo.Add(typeof(JPKDeklaracjaPozycjeSzczegolowe), "Item", new XmlAttributes() { XmlIgnore = true });
-			xo.Add(typeof(JPKDeklaracjaPozycjeSzczegolowe), "P_59", new XmlAttributes() { XmlIgnore = true });
-			xo.Add(typeof(JPKEwidencjaSprzedazWiersz), "KorektaPodstawyOpodt", new XmlAttributes() { XmlIgnore = true });
-			xo.Add(typeof(JPKEwidencjaSprzedazWiersz), "Item", new XmlAttributes() { XmlIgnore = true });
 			var xs = new XmlSerializer(typeof(JPK), xo);
 			using var xw = XmlWriter.Create(plik, new XmlWriterSettings() { OmitXmlDeclaration = false, Indent = true });
 			var nss = new XmlSerializerNamespaces();
@@ -37,20 +33,20 @@ namespace ProFak.IO.JPK_V7M
 			var jpk = new JPK();
 			jpk.Naglowek = new JPKNaglowek();
 			jpk.Naglowek.KodFormularza = new TNaglowekKodFormularza();
-			jpk.Naglowek.KodFormularza.kodSystemowy = "JPK_V7M (2)";
+			jpk.Naglowek.KodFormularza.kodSystemowy = "JPK_V7M (3)";
 			jpk.Naglowek.KodFormularza.wersjaSchemy = "1-0E";
-			jpk.Naglowek.WariantFormularza = 2;
+			jpk.Naglowek.WariantFormularza = TNaglowekWariantFormularza.Item3;
 			jpk.Naglowek.DataWytworzeniaJPK = DateTime.Now;
 			jpk.Naglowek.NazwaSystemu = "ProFak (https://github.com/lkosson/profak)";
 			jpk.Naglowek.CelZlozenia = new TNaglowekCelZlozenia();
-			jpk.Naglowek.CelZlozenia.Value = 1;
+			jpk.Naglowek.CelZlozenia.Value = TCelZlozenia.Item1;
 			jpk.Naglowek.KodUrzedu = Enum.Parse<TKodUS>("Item" + Wymagane(podmiot.KodUrzedu, "Nie uzupełniono kodu urzędu w karcie podmiotu."));
 			jpk.Naglowek.Rok = deklaracja.Miesiac.Year.ToString();
 			jpk.Naglowek.Miesiac = (sbyte)deklaracja.Miesiac.Month;
 
 			jpk.Podmiot1 = new JPKPodmiot1();
 			var jpkpodmiot = new TPodmiotDowolnyBezAdresuOsobaFizyczna();
-			jpk.Podmiot1.Item = jpkpodmiot;
+			jpk.Podmiot1.OsobaFizyczna = jpkpodmiot;
 			jpkpodmiot.NIP = Wymagane(podmiot.NIP, "Nie uzupełniono NIPu firmy.");
 			jpkpodmiot.ImiePierwsze = Wymagane(podmiot.OsobaFizycznaImie, "Nie podano imienia w karcie podmiotu.");
 			jpkpodmiot.Nazwisko = Wymagane(podmiot.OsobaFizycznaNazwisko, "Nie podano nazwiska w karcie podmiotu.");
@@ -61,8 +57,6 @@ namespace ProFak.IO.JPK_V7M
 			jpk.Ewidencja = new JPKEwidencja();
 			jpk.Ewidencja.SprzedazCtrl = new JPKEwidencjaSprzedazCtrl();
 			jpk.Ewidencja.ZakupCtrl = new JPKEwidencjaZakupCtrl();
-			var jpksprzedaze = new List<JPKEwidencjaSprzedazWiersz>();
-			var jpkzakupy = new List<JPKEwidencjaZakupWiersz>();
 			foreach (var faktura in faktury)
 			{
 				if (faktura.CzySprzedaz)
@@ -76,45 +70,45 @@ namespace ProFak.IO.JPK_V7M
 					}
 					else if (String.IsNullOrEmpty(nipnumer)) nipnumer = "BRAK";
 					var jpksprzedaz = new JPKEwidencjaSprzedazWiersz();
-					jpksprzedaze.Add(jpksprzedaz);
-					jpksprzedaz.LpSprzedazy = jpksprzedaze.Count.ToString();
+					jpk.Ewidencja.SprzedazWiersz.Add(jpksprzedaz);
+					jpksprzedaz.LpSprzedazy = (ulong)jpk.Ewidencja.SprzedazWiersz.Count;
 					jpksprzedaz.KodKrajuNadaniaTIN = nipkraj;
 					jpksprzedaz.NrKontrahenta = nipnumer;
 					jpksprzedaz.NazwaKontrahenta = faktura.NazwaNabywcy;
 					jpksprzedaz.DowodSprzedazy = faktura.Numer;
 					jpksprzedaz.DataWystawienia = faktura.DataWystawienia;
 					jpksprzedaz.DataSprzedazy = faktura.DataSprzedazy;
-					jpksprzedaz.DataSprzedazySpecified = true;
+					if (!String.IsNullOrEmpty(faktura.NumerKSeF)) jpksprzedaz.NrKSeF = faktura.NumerKSeF;
 
 					foreach (var pozycja in faktura.Pozycje)
 					{
-						if (pozycja.GTU == 1) { jpksprzedaz.GTU_01 = 1; jpksprzedaz.GTU_01Specified = true; }
-						else if (pozycja.GTU == 2) { jpksprzedaz.GTU_02 = 1; jpksprzedaz.GTU_02Specified = true; }
-						else if (pozycja.GTU == 3) { jpksprzedaz.GTU_03 = 1; jpksprzedaz.GTU_03Specified = true; }
-						else if (pozycja.GTU == 4) { jpksprzedaz.GTU_04 = 1; jpksprzedaz.GTU_04Specified = true; }
-						else if (pozycja.GTU == 5) { jpksprzedaz.GTU_05 = 1; jpksprzedaz.GTU_05Specified = true; }
-						else if (pozycja.GTU == 6) { jpksprzedaz.GTU_06 = 1; jpksprzedaz.GTU_06Specified = true; }
-						else if (pozycja.GTU == 7) { jpksprzedaz.GTU_07 = 1; jpksprzedaz.GTU_07Specified = true; }
-						else if (pozycja.GTU == 8) { jpksprzedaz.GTU_08 = 1; jpksprzedaz.GTU_08Specified = true; }
-						else if (pozycja.GTU == 9) { jpksprzedaz.GTU_09 = 1; jpksprzedaz.GTU_09Specified = true; }
-						else if (pozycja.GTU == 10) { jpksprzedaz.GTU_10 = 1; jpksprzedaz.GTU_10Specified = true; }
-						else if (pozycja.GTU == 11) { jpksprzedaz.GTU_11 = 1; jpksprzedaz.GTU_11Specified = true; }
-						else if (pozycja.GTU == 12) { jpksprzedaz.GTU_12 = 1; jpksprzedaz.GTU_12Specified = true; }
-						else if (pozycja.GTU == 13) { jpksprzedaz.GTU_13 = 1; jpksprzedaz.GTU_13Specified = true; }
+						if (pozycja.GTU == 1) { jpksprzedaz.GTU_01 = TWybor1.Item1; }
+						else if (pozycja.GTU == 2) { jpksprzedaz.GTU_02 = TWybor1.Item1; }
+						else if (pozycja.GTU == 3) { jpksprzedaz.GTU_03 = TWybor1.Item1; }
+						else if (pozycja.GTU == 4) { jpksprzedaz.GTU_04 = TWybor1.Item1; }
+						else if (pozycja.GTU == 5) { jpksprzedaz.GTU_05 = TWybor1.Item1; }
+						else if (pozycja.GTU == 6) { jpksprzedaz.GTU_06 = TWybor1.Item1; }
+						else if (pozycja.GTU == 7) { jpksprzedaz.GTU_07 = TWybor1.Item1; }
+						else if (pozycja.GTU == 8) { jpksprzedaz.GTU_08 = TWybor1.Item1; }
+						else if (pozycja.GTU == 9) { jpksprzedaz.GTU_09 = TWybor1.Item1; }
+						else if (pozycja.GTU == 10) { jpksprzedaz.GTU_10 = TWybor1.Item1; }
+						else if (pozycja.GTU == 11) { jpksprzedaz.GTU_11 = TWybor1.Item1; }
+						else if (pozycja.GTU == 12) { jpksprzedaz.GTU_12 = TWybor1.Item1; }
+						else if (pozycja.GTU == 13) { jpksprzedaz.GTU_13 = TWybor1.Item1; }
 
 						if (pozycja.StawkaVat == null) continue;
-						if (faktura.CzyWDT) { jpksprzedaz.K_21 += pozycja.WartoscNetto; }
-						else if (pozycja.StawkaVat.Skrot.ToLower().Contains("zw")) { jpksprzedaz.K_10 += pozycja.WartoscNetto; jpksprzedaz.K_10Specified = true; }
-						else if (pozycja.StawkaVat.Wartosc == 0) { jpksprzedaz.K_13 += pozycja.WartoscNetto; jpksprzedaz.K_13Specified = true; }
-						else if (pozycja.StawkaVat.Wartosc <= 5) { jpksprzedaz.K_15 += pozycja.WartoscNetto; jpksprzedaz.K_16 += pozycja.WartoscVat; }
-						else if (pozycja.StawkaVat.Wartosc <= 8) { jpksprzedaz.K_17 += pozycja.WartoscNetto; jpksprzedaz.K_18 += pozycja.WartoscVat; }
+						if (faktura.CzyWDT) { jpksprzedaz.K_21 ??= 0; jpksprzedaz.K_21 += pozycja.WartoscNetto; }
+						else if (pozycja.StawkaVat.Skrot.ToLower().Contains("zw")) { jpksprzedaz.K_10 ??= 0;  jpksprzedaz.K_10 += pozycja.WartoscNetto; }
+						else if (pozycja.StawkaVat.Wartosc == 0) { jpksprzedaz.K_13 ??= 0; jpksprzedaz.K_13 += pozycja.WartoscNetto; }
+						else if (pozycja.StawkaVat.Wartosc <= 5) { jpksprzedaz.K_15 ??= 0; jpksprzedaz.K_15 += pozycja.WartoscNetto; }
+						else if (pozycja.StawkaVat.Wartosc <= 8) { jpksprzedaz.K_17 ??= 0; jpksprzedaz.K_17 += pozycja.WartoscNetto; }
 						else { jpksprzedaz.K_19 += pozycja.WartoscNetto; jpksprzedaz.K_20 += pozycja.WartoscVat; }
 					}
 
-					if (faktura.CzyTP) { jpksprzedaz.TP = 1; jpksprzedaz.TPSpecified = true; }
-					if (faktura.ProceduraMarzy == ProceduraMarży.BiuraPodróży) { jpksprzedaz.MR_T = 1; jpksprzedaz.MR_TSpecified = true; }
-					if (faktura.ProceduraMarzy == ProceduraMarży.TowaryUżywane || faktura.ProceduraMarzy == ProceduraMarży.DziełaSztuki || faktura.ProceduraMarzy == ProceduraMarży.PrzedmiotyKolekcjonerskie) { jpksprzedaz.MR_UZ = 1; jpksprzedaz.MR_UZSpecified = true; }
-					if (faktura.ProceduraMarzy != ProceduraMarży.NieDotyczy) { jpksprzedaz.SprzedazVAT_Marza += faktura.RazemBrutto; jpksprzedaz.SprzedazVAT_MarzaSpecified = true; }
+					if (faktura.CzyTP) { jpksprzedaz.TP = TWybor1.Item1; }
+					if (faktura.ProceduraMarzy == ProceduraMarży.BiuraPodróży) { jpksprzedaz.MR_T = TWybor1.Item1; }
+					if (faktura.ProceduraMarzy == ProceduraMarży.TowaryUżywane || faktura.ProceduraMarzy == ProceduraMarży.DziełaSztuki || faktura.ProceduraMarzy == ProceduraMarży.PrzedmiotyKolekcjonerskie) { jpksprzedaz.MR_UZ = TWybor1.Item1; }
+					if (faktura.ProceduraMarzy != ProceduraMarży.NieDotyczy) { jpksprzedaz.SprzedazVAT_Marza ??= 0; jpksprzedaz.SprzedazVAT_Marza += faktura.RazemBrutto; }
 
 					jpk.Ewidencja.SprzedazCtrl.PodatekNalezny += faktura.RazemVat;
 				}
@@ -132,15 +126,15 @@ namespace ProFak.IO.JPK_V7M
 					if (faktura.CzyWNT)
 					{
 						var jpksprzedaz = new JPKEwidencjaSprzedazWiersz();
-						jpksprzedaze.Add(jpksprzedaz);
-						jpksprzedaz.LpSprzedazy = jpksprzedaze.Count.ToString();
+						jpk.Ewidencja.SprzedazWiersz.Add(jpksprzedaz);
+						jpksprzedaz.LpSprzedazy = (ulong)jpk.Ewidencja.SprzedazWiersz.Count;
 						jpksprzedaz.KodKrajuNadaniaTIN = nipkraj;
 						jpksprzedaz.NrKontrahenta = nipnumer;
 						jpksprzedaz.NazwaKontrahenta = faktura.NazwaSprzedawcy;
 						jpksprzedaz.DowodSprzedazy = faktura.Numer;
+						if (!String.IsNullOrEmpty(faktura.NumerKSeF)) jpksprzedaz.NrKSeF = faktura.NumerKSeF;
 						jpksprzedaz.DataWystawienia = faktura.DataWystawienia;
 						jpksprzedaz.DataSprzedazy = faktura.DataSprzedazy;
-						jpksprzedaz.DataSprzedazySpecified = true;
 						jpksprzedaz.K_23 = faktura.RazemNetto;
 						jpksprzedaz.K_24 = faktura.VatNaliczony;
 						jpk.Ewidencja.SprzedazCtrl.PodatekNalezny += faktura.RazemVat;
@@ -148,26 +142,26 @@ namespace ProFak.IO.JPK_V7M
 					else
 					{
 						var jpkzakup = new JPKEwidencjaZakupWiersz();
-						jpkzakupy.Add(jpkzakup);
-						jpkzakup.LpZakupu = jpkzakupy.Count.ToString();
+						jpk.Ewidencja.ZakupWiersz.Add(jpkzakup);
+						jpkzakup.LpZakupu = (ulong)jpk.Ewidencja.ZakupWiersz.Count;
 						jpkzakup.KodKrajuNadaniaTIN = nipkraj;
 						jpkzakup.NrDostawcy = nipnumer;
 						jpkzakup.NazwaDostawcy = faktura.NazwaSprzedawcy;
 						jpkzakup.DowodZakupu = faktura.Numer;
+						if (!String.IsNullOrEmpty(faktura.NumerKSeF)) jpkzakup.NrKSeF = faktura.NumerKSeF;
 						jpkzakup.DataZakupu = faktura.DataWystawienia;
 						jpkzakup.DataWplywu = faktura.DataWprowadzenia;
-						jpkzakup.DataWplywuSpecified = faktura.DataWystawienia.Date != faktura.DataWprowadzenia.Date;
-						if (faktura.ProceduraMarzy != ProceduraMarży.NieDotyczy) { jpkzakup.ZakupVAT_Marza += faktura.RazemBrutto; jpkzakup.ZakupVAT_MarzaSpecified = true; }
+						if (faktura.ProceduraMarzy != ProceduraMarży.NieDotyczy) { jpkzakup.ZakupVAT_Marza ??= 0; jpkzakup.ZakupVAT_Marza += faktura.RazemBrutto; }
 
 						if (faktura.CzyZakupSrodkowTrwalych)
 						{
-							jpkzakup.K_40 += faktura.RazemNetto;
-							jpkzakup.K_41 += faktura.VatNaliczony;
+							jpkzakup.K_40 = faktura.RazemNetto;
+							jpkzakup.K_41 = faktura.VatNaliczony;
 						}
 						else
 						{
-							jpkzakup.K_42 += faktura.RazemNetto;
-							jpkzakup.K_43 += faktura.VatNaliczony;
+							jpkzakup.K_42 = faktura.RazemNetto;
+							jpkzakup.K_43 = faktura.VatNaliczony;
 						}
 
 						jpk.Ewidencja.ZakupCtrl.PodatekNaliczony += faktura.VatNaliczony;
@@ -175,18 +169,16 @@ namespace ProFak.IO.JPK_V7M
 				}
 			}
 
-			jpk.Ewidencja.SprzedazWiersz = jpksprzedaze.ToArray();
-			jpk.Ewidencja.SprzedazCtrl.LiczbaWierszySprzedazy = jpksprzedaze.Count.ToString();
-			jpk.Ewidencja.ZakupWiersz = jpkzakupy.ToArray();
-			jpk.Ewidencja.ZakupCtrl.LiczbaWierszyZakupow = jpkzakupy.Count.ToString();
+			jpk.Ewidencja.SprzedazCtrl.LiczbaWierszySprzedazy = (ulong)jpk.Ewidencja.SprzedazWiersz.Count;
+			jpk.Ewidencja.ZakupCtrl.LiczbaWierszyZakupow = (ulong)jpk.Ewidencja.ZakupWiersz.Count;
 
 			jpk.Deklaracja = new JPKDeklaracja();
 			jpk.Deklaracja.Naglowek = new JPKDeklaracjaNaglowek();
 			jpk.Deklaracja.Naglowek.KodFormularzaDekl = new JPKDeklaracjaNaglowekKodFormularzaDekl();
-			jpk.Deklaracja.Naglowek.KodFormularzaDekl.Value = TKodFormularzaVAT7.VAT7;
+			jpk.Deklaracja.Naglowek.KodFormularzaDekl.Value = TKodFormularzaVAT7.VAT_7;
 			jpk.Deklaracja.Naglowek.KodFormularzaDekl.wersjaSchemy = "1-0E";
-			jpk.Deklaracja.Naglowek.KodFormularzaDekl.kodSystemowy = "VAT-7 (22)";
-			jpk.Deklaracja.Naglowek.WariantFormularzaDekl = 22;
+			jpk.Deklaracja.Naglowek.KodFormularzaDekl.kodSystemowy = "VAT-7 (23)";
+			jpk.Deklaracja.Naglowek.WariantFormularzaDekl = JPKDeklaracjaNaglowekWariantFormularzaDekl.Item23;
 			jpk.Deklaracja.Pouczenia = 1;
 			jpk.Deklaracja.PozycjeSzczegolowe = new JPKDeklaracjaPozycjeSzczegolowe();
 			jpk.Deklaracja.PozycjeSzczegolowe.P_10 = LiczbaLubNull(deklaracja.NettoZW);
@@ -201,20 +193,20 @@ namespace ProFak.IO.JPK_V7M
 			jpk.Deklaracja.PozycjeSzczegolowe.P_23 = LiczbaLubNull(deklaracja.NettoWNT);
 			jpk.Deklaracja.PozycjeSzczegolowe.P_24 = LiczbaLubNull(deklaracja.NaleznyWNT);
 			jpk.Deklaracja.PozycjeSzczegolowe.P_37 = LiczbaLubNull(deklaracja.NettoRazem);
-			jpk.Deklaracja.PozycjeSzczegolowe.P_38 = LiczbaLubNull(deklaracja.NaleznyRazem);
+			jpk.Deklaracja.PozycjeSzczegolowe.P_38 = (long)deklaracja.NaleznyRazem;
 			jpk.Deklaracja.PozycjeSzczegolowe.P_39 = LiczbaLubNull(deklaracja.NaliczonyPrzeniesiony);
 			jpk.Deklaracja.PozycjeSzczegolowe.P_40 = LiczbaLubNull(deklaracja.NettoSrodkiTrwale);
 			jpk.Deklaracja.PozycjeSzczegolowe.P_41 = LiczbaLubNull(deklaracja.NaliczonySrodkiTrwale);
 			jpk.Deklaracja.PozycjeSzczegolowe.P_42 = LiczbaLubNull(deklaracja.NettoPozostale);
 			jpk.Deklaracja.PozycjeSzczegolowe.P_43 = LiczbaLubNull(deklaracja.NaliczonyPozostale);
 			jpk.Deklaracja.PozycjeSzczegolowe.P_48 = LiczbaLubNull(deklaracja.NaliczonyRazem);
-			jpk.Deklaracja.PozycjeSzczegolowe.P_51 = deklaracja.DoWplaty.ToString("0", CultureInfo.InvariantCulture);
+			jpk.Deklaracja.PozycjeSzczegolowe.P_51 = (long)deklaracja.DoWplaty;
 			jpk.Deklaracja.PozycjeSzczegolowe.P_62 = LiczbaLubNull(deklaracja.DoPrzeniesienia);
 
 			return jpk;
 		}
 
 		private static string Wymagane(string wartosc, string komunikat) => String.IsNullOrWhiteSpace(wartosc) ? throw new ApplicationException(komunikat) : wartosc;
-		private static string LiczbaLubNull(decimal wartosc) => wartosc > 0 ? wartosc.ToString("0", CultureInfo.InvariantCulture) : null;
+		private static long? LiczbaLubNull(decimal wartosc) => wartosc > 0 ? (long?)wartosc : null;
 	}
 }
