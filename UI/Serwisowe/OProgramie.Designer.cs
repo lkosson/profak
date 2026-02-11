1136de0e46b4f66f8c09154e322c2c43ed685491
Aveneid <dj.exers@gmail.com>
2026-02-11 13:20:22 +0100

    Funkcja pozwalająca na sprawdzenie aktualizacji programu

--- UI/Serwisowe/OProgramie.Designer.cs
+++ UI/Serwisowe/OProgramie.Designer.cs
@@ -21,139 +21,153 @@ namespace ProFak.UI
 			base.Dispose(disposing);
 		}
 
-		#region Component Designer generated code
+        #region Component Designer generated code
 
-		/// <summary> 
-		/// Required method for Designer support - do not modify 
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OProgramie));
-			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-			this.label1 = new System.Windows.Forms.Label();
-			this.labelWersja = new System.Windows.Forms.Label();
-			this.labelSciezka = new System.Windows.Forms.Label();
-			this.labelData = new System.Windows.Forms.Label();
-			this.linkLabelStrona = new System.Windows.Forms.LinkLabel();
-			this.label2 = new System.Windows.Forms.Label();
-			this.tableLayoutPanel1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// tableLayoutPanel1
-			// 
-			this.tableLayoutPanel1.ColumnCount = 1;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-			this.tableLayoutPanel1.Controls.Add(this.labelWersja, 0, 1);
-			this.tableLayoutPanel1.Controls.Add(this.labelSciezka, 0, 2);
-			this.tableLayoutPanel1.Controls.Add(this.labelData, 0, 3);
-			this.tableLayoutPanel1.Controls.Add(this.linkLabelStrona, 0, 4);
-			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 5);
-			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 7;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(494, 281);
-			this.tableLayoutPanel1.TabIndex = 0;
-			// 
-			// label1
-			// 
-			this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.label1.Location = new System.Drawing.Point(207, 10);
-			this.label1.Margin = new System.Windows.Forms.Padding(10);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(79, 30);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "ProFak";
-			// 
-			// labelWersja
-			// 
-			this.labelWersja.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.labelWersja.AutoSize = true;
-			this.labelWersja.Location = new System.Drawing.Point(222, 60);
-			this.labelWersja.Margin = new System.Windows.Forms.Padding(10);
-			this.labelWersja.Name = "labelWersja";
-			this.labelWersja.Size = new System.Drawing.Size(50, 15);
-			this.labelWersja.TabIndex = 0;
-			this.labelWersja.Text = "[Wersja]";
-			// 
-			// labelSciezka
-			// 
-			this.labelSciezka.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.labelSciezka.AutoSize = true;
-			this.labelSciezka.Location = new System.Drawing.Point(220, 95);
-			this.labelSciezka.Margin = new System.Windows.Forms.Padding(10);
-			this.labelSciezka.Name = "labelSciezka";
-			this.labelSciezka.Size = new System.Drawing.Size(53, 15);
-			this.labelSciezka.TabIndex = 0;
-			this.labelSciezka.Text = "[Ścieżka]";
-			// 
-			// labelData
-			// 
-			this.labelData.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.labelData.AutoSize = true;
-			this.labelData.Location = new System.Drawing.Point(227, 130);
-			this.labelData.Margin = new System.Windows.Forms.Padding(10);
-			this.labelData.Name = "labelData";
-			this.labelData.Size = new System.Drawing.Size(39, 15);
-			this.labelData.TabIndex = 0;
-			this.labelData.Text = "[Data]";
-			// 
-			// linkLabelStrona
-			// 
-			this.linkLabelStrona.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.linkLabelStrona.AutoSize = true;
-			this.linkLabelStrona.Location = new System.Drawing.Point(150, 165);
-			this.linkLabelStrona.Margin = new System.Windows.Forms.Padding(10);
-			this.linkLabelStrona.Name = "linkLabelStrona";
-			this.linkLabelStrona.Size = new System.Drawing.Size(193, 15);
-			this.linkLabelStrona.TabIndex = 1;
-			this.linkLabelStrona.TabStop = true;
-			this.linkLabelStrona.Text = "https://github.com/lkosson/profak";
-			this.linkLabelStrona.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelStrona_LinkClicked);
-			// 
-			// label2
-			// 
-			this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(18, 200);
-			this.label2.Margin = new System.Windows.Forms.Padding(10);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(458, 60);
-			this.label2.TabIndex = 0;
-			this.label2.Text = resources.GetString("label2.Text");
-			// 
-			// OProgramie
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.tableLayoutPanel1);
-			this.Name = "OProgramie";
-			this.Size = new System.Drawing.Size(494, 281);
-			this.tableLayoutPanel1.ResumeLayout(false);
-			this.tableLayoutPanel1.PerformLayout();
-			this.ResumeLayout(false);
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OProgramie));
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            label1 = new System.Windows.Forms.Label();
+            labelWersja = new System.Windows.Forms.Label();
+            labelSciezka = new System.Windows.Forms.Label();
+            labelData = new System.Windows.Forms.Label();
+            linkLabelStrona = new System.Windows.Forms.LinkLabel();
+            label2 = new System.Windows.Forms.Label();
+            btnSprawdzAktualizacje = new System.Windows.Forms.Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(labelWersja, 0, 1);
+            tableLayoutPanel1.Controls.Add(labelSciezka, 0, 2);
+            tableLayoutPanel1.Controls.Add(labelData, 0, 3);
+            tableLayoutPanel1.Controls.Add(linkLabelStrona, 0, 4);
+            tableLayoutPanel1.Controls.Add(label2, 0, 5);
+            tableLayoutPanel1.Controls.Add(btnSprawdzAktualizacje, 0, 6);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 8;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 13F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(494, 319);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 16F);
+            label1.Location = new System.Drawing.Point(208, 10);
+            label1.Margin = new System.Windows.Forms.Padding(10);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(78, 30);
+            label1.TabIndex = 0;
+            label1.Text = "ProFak";
+            // 
+            // labelWersja
+            // 
+            labelWersja.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            labelWersja.AutoSize = true;
+            labelWersja.Location = new System.Drawing.Point(222, 60);
+            labelWersja.Margin = new System.Windows.Forms.Padding(10);
+            labelWersja.Name = "labelWersja";
+            labelWersja.Size = new System.Drawing.Size(50, 15);
+            labelWersja.TabIndex = 0;
+            labelWersja.Text = "[Wersja]";
+            // 
+            // labelSciezka
+            // 
+            labelSciezka.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            labelSciezka.AutoSize = true;
+            labelSciezka.Location = new System.Drawing.Point(220, 95);
+            labelSciezka.Margin = new System.Windows.Forms.Padding(10);
+            labelSciezka.Name = "labelSciezka";
+            labelSciezka.Size = new System.Drawing.Size(53, 15);
+            labelSciezka.TabIndex = 0;
+            labelSciezka.Text = "[Ścieżka]";
+            // 
+            // labelData
+            // 
+            labelData.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            labelData.AutoSize = true;
+            labelData.Location = new System.Drawing.Point(227, 130);
+            labelData.Margin = new System.Windows.Forms.Padding(10);
+            labelData.Name = "labelData";
+            labelData.Size = new System.Drawing.Size(39, 15);
+            labelData.TabIndex = 0;
+            labelData.Text = "[Data]";
+            // 
+            // linkLabelStrona
+            // 
+            linkLabelStrona.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            linkLabelStrona.AutoSize = true;
+            linkLabelStrona.Location = new System.Drawing.Point(150, 165);
+            linkLabelStrona.Margin = new System.Windows.Forms.Padding(10);
+            linkLabelStrona.Name = "linkLabelStrona";
+            linkLabelStrona.Size = new System.Drawing.Size(193, 15);
+            linkLabelStrona.TabIndex = 1;
+            linkLabelStrona.TabStop = true;
+            linkLabelStrona.Text = "https://github.com/lkosson/profak";
+            linkLabelStrona.LinkClicked += linkLabelStrona_LinkClicked;
+            // 
+            // label2
+            // 
+            label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(18, 200);
+            label2.Margin = new System.Windows.Forms.Padding(10);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(457, 60);
+            label2.TabIndex = 0;
+            label2.Text = resources.GetString("label2.Text");
+            // 
+            // btnSprawdzAktualizacje
+            // 
+            btnSprawdzAktualizacje.Location = new System.Drawing.Point(3, 273);
+            btnSprawdzAktualizacje.Name = "btnSprawdzAktualizacje";
+            btnSprawdzAktualizacje.Size = new System.Drawing.Size(134, 30);
+            btnSprawdzAktualizacje.TabIndex = 2;
+            btnSprawdzAktualizacje.Text = "Sprawdź aktualiazcje";
+            btnSprawdzAktualizacje.UseVisualStyleBackColor = true;
+            btnSprawdzAktualizacje.Click += btnSprawdzAktualizacje_Click;
+            // 
+            // OProgramie
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel1);
+            Name = "OProgramie";
+            Size = new System.Drawing.Size(494, 319);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
 
-		}
+        }
 
-		#endregion
+        #endregion
 
-		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label labelWersja;
 		private System.Windows.Forms.Label labelSciezka;
 		private System.Windows.Forms.Label labelData;
 		private System.Windows.Forms.LinkLabel linkLabelStrona;
 		private System.Windows.Forms.Label label2;
-	}
+        private System.Windows.Forms.Button btnSprawdzAktualizacje;
+    }
 }
--- UI/Serwisowe/OProgramie.cs
+++ UI/Serwisowe/OProgramie.cs
@@ -6,28 +6,59 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace ProFak.UI
 {
-	partial class OProgramie : UserControl, IKontrolkaZKontekstem
-	{
-		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-		public Kontekst Kontekst { get; set; }
+    partial class OProgramie : UserControl, IKontrolkaZKontekstem
+    {
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Kontekst Kontekst { get; set; }
 
-		public OProgramie()
-		{
-			InitializeComponent();
-			labelWersja.Text = GetType().Assembly.GetName().Version.ToString();
-			labelSciezka.Text = Environment.ProcessPath;
-			labelData.Text = File.GetLastWriteTime(Environment.ProcessPath).ToString("d MMMM yyyy, H:mm:ss");
-		}
+        public OProgramie()
+        {
+            InitializeComponent();
+            labelWersja.Text = GetType().Assembly.GetName().Version.ToString();
+            labelSciezka.Text = Environment.ProcessPath;
+            labelData.Text = File.GetLastWriteTime(Environment.ProcessPath).ToString("d MMMM yyyy, H:mm:ss");
+        }
 
-		private void linkLabelStrona_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-		{
-			Process.Start(new ProcessStartInfo { UseShellExecute = true, FileName = "https://github.com/lkosson/profak/" });
-		}
-	}
+        private void linkLabelStrona_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo { UseShellExecute = true, FileName = "https://github.com/lkosson/profak/" });
+        }
+
+        private void btnSprawdzAktualizacje_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var wb = new WebClient();
+                wb.Headers.Add("User-Agent: Other"); // fix for HTTP 403
+                var response = wb.DownloadString("https://api.github.com/repos/lkosson/profak/releases/latest");
+                var json = JObject.Parse(response);
+                Version wersjaGitHub = Version.Parse(json["tag_name"].ToString().Replace("v", ""));
+                Version wersjaAplikacji = GetType().Assembly.GetName().Version;
+                if (wersjaGitHub.Major > wersjaAplikacji.Major ||
+                    (wersjaGitHub.Major == wersjaAplikacji.Major && wersjaGitHub.Minor > wersjaAplikacji.Minor))
+                {
+                    if (MessageBox.Show("Dostępna jest nowa wersja " + json["tag_name"] + ". \r\nCzy chcesz przejść do strony pobierania?", "Aktualizacja programu", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start("explorer.exe", json["html_url"].ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nie znaleziono nowej wersji oprogramowania.", "Aktualizacja programu");
+                }
+            }
+            catch (Exception ex)
+            {
+                OknoBledu.Pokaz(ex);
+            }
+        }
+    }
 }
