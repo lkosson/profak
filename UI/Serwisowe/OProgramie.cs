6782fc6632ceb1b47129a161e27494bd32a9f4a1
veneid <dj.exers@gmail.com>
2026-02-12 10:25:08 +0100

    #90 - Funkcja pozwalająca na sprawdzenie aktualizacji programu

--- UI/Serwisowe/OProgramie.cs
+++ UI/Serwisowe/OProgramie.cs
@@ -6,9 +6,12 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.Json;
 
 namespace ProFak.UI
 {
@@ -28,6 +31,47 @@ namespace ProFak.UI
 		private void linkLabelStrona_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
 			Process.Start(new ProcessStartInfo { UseShellExecute = true, FileName = "https://github.com/lkosson/profak/" });
+		}
+
+		private void btnSprawdzAktualizacje_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				string response = null;
+				OknoPostepu.Uruchom(async cancellationToken =>
+				{
+					using var wb = new HttpClient();
+					wb.DefaultRequestHeaders.UserAgent.ParseAdd("ProFak (https://github.com/lkosson/profak)");
+					response = await wb.GetStringAsync("https://api.github.com/repos/lkosson/profak/releases/latest",cancellationToken);
+					cancellationToken.ThrowIfCancellationRequested();
+				});
+				
+				
+				var json = JsonDocument.Parse(response);
+				Version wersjaGitHub;
+				Version.TryParse(json.RootElement.GetProperty("tag_name").ToString().Replace("v", ""),out wersjaGitHub);
+				Version wersjaAplikacji = GetType().Assembly.GetName().Version;
+				if(wersjaGitHub == null)
+				{
+					MessageBox.Show("Nie znaleziono nowej wersji programu", "ProFak", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					return;
+				}
+				if (wersjaGitHub.Major > wersjaAplikacji.Major ||
+					(wersjaGitHub.Major == wersjaAplikacji.Major && wersjaGitHub.Minor > wersjaAplikacji.Minor))
+				{
+					if (MessageBox.Show("Dostępna jest nowa wersja " + wersjaGitHub.ToString() + ". \r\nCzy chcesz przejść do strony pobierania?", "ProFak", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+						
+					{
+						Process.Start(new ProcessStartInfo { UseShellExecute = true, FileName = json.RootElement.GetProperty("html_url").ToString() });
+					}
+				} else {
+					MessageBox.Show("Nie znaleziono nowej wersji programu", "ProFak", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+			}
+			catch (Exception ex)
+			{
+				OknoBledu.Pokaz(ex);
+			}
 		}
 	}
 }
