8493285338b71c1a4678a083a2273fdfbaad7244
Łukasz Kosson <git@lukasz.kosson.net>
2026-02-16 15:08:48 +0100

    Dodawanie wielu faktur KSeF z plików

--- UI/Faktury/WczytajKSeFAkcja.cs
+++ UI/Faktury/WczytajKSeFAkcja.cs
@@ -3,36 +3,63 @@ using System.Text.RegularExpressions;
 
 namespace ProFak.UI;
 
-class WczytajKSeFAkcja : DodajRekordAkcja<Faktura, FakturaEdytor>
+class WczytajKSeFAkcja : AkcjaNaSpisie<Faktura>
 {
 	public override string Nazwa => "➕ Wczytaj XML KSeF [CTRL-K]";
 	public override bool CzyDostepnaDlaRekordow(IEnumerable<Faktura> zaznaczoneRekordy) => true;
 	public override bool CzyKlawiszSkrotu(Keys klawisz, Keys modyfikatory) => klawisz == Keys.K && modyfikatory == Keys.Control;
 
-	protected override Faktura? UtworzRekord(Kontekst kontekst, IEnumerable<Faktura> zaznaczoneRekordy)
+	public override void Uruchom(Kontekst kontekst, ref IEnumerable<Faktura> zaznaczoneRekordy)
 	{
 		using var dialog = new OpenFileDialog();
 		dialog.Filter = "e-Faktura XML (*.xml)|*.xml|Wszystkie pliki (*.*)|*.*";
 		dialog.Title = "Wybierz e-Fakturę do załadowania";
 		dialog.RestoreDirectory = true;
-		if (dialog.ShowDialog() != DialogResult.OK) return null;
+		dialog.Multiselect = true;
+		if (dialog.ShowDialog() != DialogResult.OK) return;
 
-		var podmiot = kontekst.Baza.Kontrahenci.First(kontrahent => kontrahent.CzyPodmiot);
-		var xml = File.ReadAllText(dialog.FileName);
+		var rekordy = new List<Faktura>();
+		var pliki = dialog.FileNames;
+		for (var i = 0; i < pliki.Length; i++)
+		{
+			var plik = pliki[i];
+			using var nowyKontekst = new Kontekst(kontekst);
+			using var transakcja = nowyKontekst.Transakcja();
+			var faktura = DodajFakture(nowyKontekst, plik);
+			if (faktura == null)
+			{
+				if (i < pliki.Length - 1 && MessageBox.Show("Kontynuować dodawanie faktur ze wskazanych plików?", "ProFak", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) != DialogResult.Yes)
+					break;
+				continue;
+			}
+			transakcja.Zatwierdz();
+			rekordy.Add(faktura);
+		}
+		zaznaczoneRekordy = rekordy;
+	}
+
+	private Faktura? DodajFakture(Kontekst kontekst, string plik)
+	{
+		var xml = File.ReadAllText(plik);
 		var faktura = IO.FA_3.Generator.ZbudujDB(kontekst.Baza, xml);
 		faktura.DataKSeF = DateTime.Now;
-		var plik = Path.GetFileNameWithoutExtension(dialog.FileName);
-		if (Regex.IsMatch(plik, @"\d{10}-\d{8}-[0-9A-Fa-f]{12}-[0-9A-Fa-f]{2}"))
+		var numerKsef = Path.GetFileNameWithoutExtension(plik);
+		if (Regex.IsMatch(numerKsef, @"\d{10}-\d{8}-[0-9A-Fa-f]{12}-[0-9A-Fa-f]{2}"))
 		{
-			var istniejaca = kontekst.Baza.Faktury.FirstOrDefault(e => e.NumerKSeF == plik);
-			if (istniejaca != null)
-			{
-				if (MessageBox.Show($"Faktura {istniejaca.Numer} ({istniejaca.NumerKSeF}) już istnieje w bazie. Czy mimo to chcesz ją dodać ponownie?", "ProFak", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) != DialogResult.Yes) return null;
-			}
-			faktura.NumerKSeF = plik;
+			var istniejaca = kontekst.Baza.Faktury.FirstOrDefault(e => e.NumerKSeF == numerKsef);
+			if (istniejaca != null && MessageBox.Show($"Faktura {istniejaca.Numer} ({istniejaca.NumerKSeF}) już istnieje w bazie. Czy mimo to chcesz ją dodać ponownie?", "ProFak", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) != DialogResult.Yes) 
+				return null;
+			faktura.NumerKSeF = numerKsef;
 		}
 		kontekst.Baza.Zapisz(faktura);
-		IO.FA_3.Generator.PoprawPowiazaniaPoZapisie(kontekst.Baza, faktura);
+
+		kontekst.Dodaj(faktura);
+		using var edytor = new FakturaEdytor();
+		using var okno = new Dialog("Nowa pozycja", edytor, kontekst);
+		edytor.Przygotuj(kontekst, faktura);
+		if (okno.ShowDialog() != DialogResult.OK) return null;
+		edytor.KoniecEdycji();
+		kontekst.Baza.Zapisz(faktura);
 		return faktura;
 	}
 }
