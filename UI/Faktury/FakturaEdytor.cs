d28c760d30a870e17121ce0aa8be42093e0202fc
Łukasz Kosson <git@lukasz.kosson.net>
2026-02-27 13:35:28 +0100

    #98 - Cofnięty wybór domyślnej waluty gdy nie ma ustawionej waluty kontrahenta

--- UI/Faktury/FakturaEdytor.cs
+++ UI/Faktury/FakturaEdytor.cs
@@ -222,13 +222,7 @@ partial class FakturaEdytor : FakturaEdytorBase
 		if (kontrahent == null || rekord.NabywcaRef == kontrahent.Ref) return false;
 		rekord.UstawNabywce(kontrahent);
 		if (kontrahent.SposobPlatnosciRef.IsNotNull) comboBoxSposobPlatnosci.SelectedValue = kontrahent.SposobPlatnosciRef;
-
-		if (kontrahent.DomyslnaWalutaRef.IsNotNull)
-			comboBoxWaluta.SelectedValue = kontrahent.DomyslnaWalutaRef;
-		else
-		{
-			comboBoxWaluta.SelectedValue = Kontekst.Baza.Waluty.FirstOrDefault(waluta => waluta.CzyDomyslna).Ref;
-		}
+		if (kontrahent.DomyslnaWalutaRef.IsNotNull) comboBoxWaluta.SelectedValue = kontrahent.DomyslnaWalutaRef;
 		return true;
 	}
 
