59322b4ee1cb87bbed174dc6e8bc408af25501c4
Łukasz Kosson <git@lukasz.kosson.net>
2026-04-04 18:12:18 +0200

    Kontrolka zakładek

---
+++ UI/Zakladki.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProFak.UI;
+
+class Zakladki : TabControl
+{
+	public void Dodaj(string etykieta, Control zawartosc)
+	{
+		var tabPage = new TabPage();
+		tabPage.Text = etykieta;
+		tabPage.Padding = new Padding(3);
+		tabPage.UseVisualStyleBackColor = true;
+		tabPage.Controls.Add(zawartosc);
+		zawartosc.Dock = DockStyle.Fill;
+		TabPages.Add(tabPage);
+	}
+}
