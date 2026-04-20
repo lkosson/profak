aa8041962ccc88fea855004d7ad0468a752be754
Łukasz Kosson <git@lukasz.kosson.net>
2026-04-20 18:53:22 +0200

    Wydzielona wyszukiwarka specyficzna dla WinForms

---
+++ UI/WinForms/Wyszukiwarka.cs
@@ -0,0 +1,39 @@
+﻿#if WINFORMS
+namespace ProFak.UI;
+
+partial class Wyszukiwarka<TRekord> : TextBox
+{
+	public Wyszukiwarka(Spis<TRekord> spis)
+	{
+		this.spis = spis;
+		var opis = "";
+		if (Wyglad.IkonyAkcji) opis += "🔍 ";
+		opis += "Wyszukaj";
+		if (Wyglad.SkrotyKlawiaturoweAkcji) opis += " [F3]";
+		PlaceholderText = opis;
+		TextAlign = HorizontalAlignment.Center;
+	}
+
+	protected override void OnTextChanged(EventArgs e)
+	{
+		base.OnTextChanged(e);
+		UstawFiltr(Text);
+		TextAlign = String.IsNullOrEmpty(Text) ? HorizontalAlignment.Center : HorizontalAlignment.Left;
+	}
+
+	protected override void OnKeyDown(KeyEventArgs e)
+	{
+		if (e.KeyCode == Keys.Escape)
+		{
+			Text = "";
+			spis.Focus();
+			e.Handled = true;
+		}
+		else if (e.KeyCode == Keys.Enter)
+		{
+			if (spis.Rekordy.Any()) spis.Focus();
+			e.Handled = true;
+		}
+	}
+}
+#endif
