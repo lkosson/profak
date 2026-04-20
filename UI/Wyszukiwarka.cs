26014b78b37c850dec472b138ccd7d71fa100386
Łukasz Kosson <git@lukasz.kosson.net>
2026-04-20 18:53:22 +0200

    Wydzielona wyszukiwarka specyficzna dla WinForms

--- UI/Wyszukiwarka.cs
+++ UI/Wyszukiwarka.cs
@@ -3,43 +3,10 @@ using System.Text.RegularExpressions;
 
 namespace ProFak.UI;
 
-class Wyszukiwarka<TRekord> : TextBox
+partial class Wyszukiwarka<TRekord>
 	where TRekord : Rekord<TRekord>
 {
 	private Spis<TRekord> spis;
-
-	public Wyszukiwarka(Spis<TRekord> spis)
-	{
-		this.spis = spis;
-		var opis = "";
-		if (Wyglad.IkonyAkcji) opis += "🔍 ";
-		opis += "Wyszukaj";
-		if (Wyglad.SkrotyKlawiaturoweAkcji) opis += " [F3]";
-		PlaceholderText = opis;
-		TextAlign = HorizontalAlignment.Center;
-	}
-
-	protected override void OnTextChanged(EventArgs e)
-	{
-		base.OnTextChanged(e);
-		UstawFiltr(Text);
-		TextAlign = String.IsNullOrEmpty(Text) ? HorizontalAlignment.Center : HorizontalAlignment.Left;
-	}
-
-	protected override void OnKeyDown(KeyEventArgs e)
-	{
-		if (e.KeyCode == Keys.Escape)
-		{
-			Text = "";
-			spis.Focus();
-			e.Handled = true;
-		}
-		else if (e.KeyCode == Keys.Enter)
-		{
-			if (spis.Rekordy.Any()) spis.Focus();
-			e.Handled = true;
-		}
-	}
 
 	private void UstawFiltr(string wyrazenieFiltra)
 	{
