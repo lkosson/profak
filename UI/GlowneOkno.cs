b6a90b64794d0bd83715fd06de4ce780e52b542a
Łukasz Kosson <git@lukasz.kosson.net>
2026-04-20 18:37:54 +0200

    Wydzielone okno główne specyficzne dla WinForms

--- UI/GlowneOkno.cs
+++ UI/GlowneOkno.cs
@@ -7,27 +7,9 @@ using System.Reflection;
 
 namespace ProFak.UI;
 
-class GlowneOkno : Form
+partial class GlowneOkno
 {
 	private Menu menu;
-	private Panel panelZawartosc;
-
-	public GlowneOkno()
-	{
-		menu = new Menu(ZbudujMenu);
-
-		panelZawartosc = new Panel();
-
-		var uklad = new Siatka([Wyglad.SzerokoscMenu, -1], [-1]);
-		uklad.DodajWiersz([menu, panelZawartosc]);
-
-		KeyPreview = true;
-		WindowState = FormWindowState.Maximized;
-		Text = "ProFak";
-		Icon = Ikona;
-		uklad.Dock = DockStyle.Fill;
-		Controls.Add(uklad);
-	}
 
 	public static Icon? Ikona
 	{
@@ -38,18 +20,6 @@ class GlowneOkno : Form
 			if (dane == null) return null;
 			return new Icon(dane);
 		}
-	}
-
-	protected override void OnFormClosing(FormClosingEventArgs e)
-	{
-		if (e.CloseReason == CloseReason.UserClosing && Wyglad.PotwierdzanieZamknieciaProgramu)
-		{
-			if (!OknoKomunikatu.PytanieTakNie("Czy na pewno chcesz zamknąć program?", domyslnie: false))
-			{
-				e.Cancel = true;
-			}
-		}
-		base.OnFormClosing(e);
 	}
 
 	private TTreeNode[] ZbudujMenu()
@@ -130,28 +100,6 @@ class GlowneOkno : Form
 		return [faktury, rachunkiSprzedazy, podatki, kontrahenci, towary, slowniki, serwisowe];
 	}
 
-	private void Wyswietl<TKontrolka>(TKontrolka kontrolka)
-		where TKontrolka : Control, IKontrolkaZKontekstem
-	{
-		var doUsuniecia = panelZawartosc.Controls.Cast<Control>().ToList();
-
-		var kontekst = new Kontekst();
-		kontrolka.Kontekst = kontekst;
-		kontrolka.Disposed += delegate { panelZawartosc.Controls.Remove(kontrolka); menu.Focus(); kontekst.Dispose(); };
-		kontrolka.Dock = DockStyle.Fill;
-		panelZawartosc.Controls.Add(kontrolka);
-		kontrolka.BringToFront();
-
-		if (ModifierKeys != Keys.Control)
-		{
-			foreach (var istniejace in doUsuniecia)
-			{
-				istniejace.Dispose();
-			}
-		}
-
-		kontrolka.Focus();
-	}
 
 	private TTreeNode[] WypelnijDatyFaktur(Expression<Func<Faktura, bool>> warunek, Action<DateTime, DateTime> akcja)
 	{
