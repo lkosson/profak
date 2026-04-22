2538c4e94b60bc32f7a14bb650f95af1972a518a
Łukasz Kosson <git@lukasz.kosson.net>
2026-04-22 09:29:33 +0200

    Wydzielone podsumowanie specyficzne dla WinForms

---
+++ UI/WinForms/Podsumowanie.cs
@@ -0,0 +1,66 @@
+﻿#if WINFORMS
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ProFak.UI;
+
+partial class Podsumowanie : LinkLabel
+{
+	public Podsumowanie()
+	{
+		AutoSize = true;
+		LinkBehavior = LinkBehavior.HoverUnderline;
+		Links.Clear();
+	}
+
+	[AllowNull]
+	public override string Text
+	{
+		get => base.Text;
+		set
+		{
+			var odnosniki = new List<(int poczatek, string wartosc)>();
+			if (value != null)
+			{
+				var sb = new StringBuilder(value.Length);
+				int? poczatek = null;
+				for (var i = 0; i < value.Length; i++)
+				{
+					var ch = value[i];
+					if (ch == '<')
+					{
+						poczatek = i + 1;
+					}
+					else if (ch == '>' && poczatek is not null)
+					{
+						var odnosnik = value[poczatek.Value..i];
+						odnosniki.Add((sb.Length, odnosnik));
+						sb.Append(odnosnik);
+						poczatek = null;
+					}
+					else if (poczatek is null)
+					{
+						sb.Append(ch);
+					}
+				}
+				base.Text = sb.ToString();
+			}
+			else
+			{
+				base.Text = "";
+			}
+			Links.Clear();
+			foreach (var odnosnik in odnosniki)
+				Links.Add(odnosnik.poczatek, odnosnik.wartosc.Length, odnosnik.wartosc);
+		}
+	}
+
+	protected override void OnLinkClicked(LinkLabelLinkClickedEventArgs e)
+	{
+		if (e.Link?.LinkData is not string tekst) return;
+		if (Regex.IsMatch(tekst, @"[0-9,\s]+")) tekst = tekst.Replace("\u00A0", "");
+		Clipboard.SetText(tekst);
+	}
+}
+#endif
