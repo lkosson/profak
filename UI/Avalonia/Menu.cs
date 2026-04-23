9bda1e394cebabe61948a7e1ebff9651496ee0be
Łukasz Kosson <git@lukasz.kosson.net>
2026-04-23 07:35:48 +0200

    Główne menu Avalonia

--- UI/Avalonia/Menu.cs
+++ UI/Avalonia/Menu.cs
@@ -10,6 +10,10 @@ namespace ProFak.UI;
 partial class Menu : Avalonia.Controls.TreeView
 {
 	public ObservableCollection<TTreeNode> Nodes { get; } = [];
+	public TTreeNode? SelectedNode {
+		get => (TTreeNode?)((TreeViewItem?)SelectedItem)?.DataContext;
+		set { /* TODO Avalonia */ }
+	}
 
 	private void Zbuduj(bool pokazUkryte = false)
 	{
@@ -30,6 +34,7 @@ partial class Menu : Avalonia.Controls.TreeView
 		Zbuduj();
 		AddHandler(TreeViewItem.ExpandedEvent, Expand);
 		AddHandler(TreeViewItem.CollapsedEvent, Collapse);
+		SelectionChanged += Menu_SelectionChanged;
 		menuGotowe = true;
 	}
 
@@ -58,43 +63,52 @@ partial class Menu : Avalonia.Controls.TreeView
 
 	private void Collapse(object? sender, RoutedEventArgs e)
 	{
-		var treeNode = (TreeViewItem)e.Source!;
-		ZapiszStanPozycji(treeNode);
+		var item = (TreeViewItem)e.Source!;
+		if (item == null) return;
+		var wezel = (TTreeNode)item.DataContext!;
+		ZapiszStanPozycji(wezel);
 	}
 
-	protected override void OnAfterSelect(TreeViewEventArgs e)
+	private void Menu_SelectionChanged(object? sender, SelectionChangedEventArgs e)
 	{
-		base.OnAfterSelect(e);
 		if (trwaAktualizacjaMenu) return;
-		if ((e.Action & TreeViewAction.ByKeyboard) == TreeViewAction.ByKeyboard) return;
 		var wybrany = SelectedNode;
 		if (wybrany == null) return;
-		if (ostatnioWybrany == null || ostatnioWybrany.TreeView == null || !ostatnioWybrany.FullPath.StartsWith(wybrany.FullPath)) while (wybrany.Nodes.Count > 0) wybrany = wybrany.Nodes[0];
+		while (wybrany.Nodes.Count > 0) wybrany = wybrany.Nodes[0];
 		if (SelectedNode == wybrany) Wyswietl(wybrany);
 		else SelectedNode = wybrany;
 
 		ZapiszStanPozycji(SelectedNode /* dla spisu według kontrahentów/towarów/dat mogło się zmienić */);
 	}
 
-	protected override void OnKeyPress(KeyPressEventArgs e)
+	protected override void OnKeyDown(Avalonia.Input.KeyEventArgs e)
 	{
-		base.OnKeyPress(e);
+		base.OnKeyDown(e);
 		if (SelectedNode == null) return;
-		if (e.KeyChar == '\r')
+		if (e.Key == Avalonia.Input.Key.Enter)
 		{
 			e.Handled = true;
 			Wyswietl(SelectedNode);
 		}
-		else if (e.KeyChar == '/')
+		else if (e.Key == Avalonia.Input.Key.OemQuestion)
 		{
 			e.Handled = true;
-			SelectedNode.Collapse(false);
+			// TODO Avalonia
+			//SelectedNode.Collapse(false);
 		}
 	}
+	// TODO Avalonia
+	/*
 	protected override void OnNodeMouseClick(TreeNodeMouseClickEventArgs e)
 	{
 		base.OnNodeMouseClick(e);
 		if (e.Button == MouseButtons.Right && e.Node != null) PokazMenuKontekstowe(e.Node);
+	}
+	*/
+
+	private void Rozwin(bool pokazUkryte)
+	{
+		// TODO Avalonia
 	}
 
 	private void Wyswietl(TTreeNode wezel)
