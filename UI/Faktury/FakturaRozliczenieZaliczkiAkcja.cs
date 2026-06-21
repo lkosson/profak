5fdf5a4a502426436966dfe327198976fb5e100b
Łukasz Kosson <git@lukasz.kosson.net>
2026-06-21 12:59:04 +0200

    #59 - Zaliczki jako wpłaty na rozliczającej

--- UI/Faktury/FakturaRozliczenieZaliczkiAkcja.cs
+++ UI/Faktury/FakturaRozliczenieZaliczkiAkcja.cs
@@ -17,6 +17,8 @@ class FakturaRozliczenieZaliczkiAkcja : FakturaPodobnaAkcja
 		{
 			zaliczka.FakturaRozliczeniowaId = podobna.Id;
 			kontekst.Baza.Zapisz(zaliczka);
+			var wplata = new Wplata { Data = zaliczka.DataWystawienia, FakturaRef = podobna, Kwota = zaliczka.RazemBrutto, Uwagi = $"Zaliczka {zaliczka.Numer}", CzyRozliczenie = true };
+			kontekst.Baza.Zapisz(wplata);
 		}
 		return podobna;
 	}
