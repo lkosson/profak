af1b850416f88b3f4cd710fb87585eeab90b08e9
Łukasz Kosson <git@lukasz.kosson.net>
2026-09-02 05:49:47 +0200

    #154 - Bardziej czytelna kolorystyka spisów Avalonia

--- UI/Avalonia/Interfejs.cs
+++ UI/Avalonia/Interfejs.cs
@@ -4,6 +4,9 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml.Styling;
 using Avalonia.Themes.Simple;
 using Avalonia.Platform.Storage;
+using Avalonia.Styling;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
 
 namespace ProFak.UI;
 
@@ -19,6 +22,18 @@ class Interfejs
 		application = appBuilder.Instance!;
 		application.Styles.Add(new SimpleTheme());
 		application.Styles.Add(new StyleInclude(new Uri("avares://ProFak")) { Source = new Uri("avares://Avalonia.Controls.DataGrid/Themes/Simple.xaml") });
+
+		var stylCheckbox = new Style(x => x.OfType<TCheckBox>().Class(":checked").Template().OfType<Avalonia.Controls.Shapes.Path>());
+		stylCheckbox.Setters.Add(new Setter(Shape.FillProperty, Brushes.Black));
+		application.Styles.Add(stylCheckbox);
+
+		var stylDataGridWybrany = new Style(x => x.OfType<DataGridRow>().Template().OfType<Rectangle>().Name("BackgroundRectangle"));
+		stylDataGridWybrany.Setters.Add(new Setter(Shape.FillProperty, new SolidColorBrush(TColor.FromRgb(50, 160, 245))));
+		application.Styles.Add(stylDataGridWybrany);
+
+		var stylDataGridTlo = new Style(x => x.OfType<DataGridRow>());
+		stylDataGridTlo.Setters.Add(new Setter(DataGridRow.BackgroundProperty, Brushes.White));
+		application.Styles.Add(stylDataGridTlo);
 	}
 
 	public static void Wyswietl(Window okno)
